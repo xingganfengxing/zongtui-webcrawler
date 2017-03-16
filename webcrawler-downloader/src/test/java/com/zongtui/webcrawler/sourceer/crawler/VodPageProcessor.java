cf2ba7d42fb33873bdfbf8a3b725134cf895e9f9
Administrator <Administrator@USER-20160925MP>
2017-03-16 14:45:50 +0800

    ii

--- webcrawler-downloader/src/test/java/com/zongtui/webcrawler/sourceer/crawler/VodPageProcessor.java
+++ webcrawler-downloader/src/test/java/com/zongtui/webcrawler/sourceer/crawler/VodPageProcessor.java
@@ -3,23 +3,19 @@ package com.zongtui.webcrawler.sourceer.crawler;
 import java.util.List;
 
 import com.zongtui.webcrawler.sourceer.Page;
-import com.zongtui.webcrawler.sourceer.ResultItems;
 import com.zongtui.webcrawler.sourceer.Site;
 import com.zongtui.webcrawler.sourceer.Spider;
 import com.zongtui.webcrawler.sourceer.processor.PageProcessor;
 
-/**
- * @author code4crafter@gmail.com <br>
- */
-public class OschinaBlogPageProcessor implements PageProcessor {
+public class VodPageProcessor implements PageProcessor{
 
-    private Site site = Site.me().setDomain("my.oschina.net");
+    private Site site = Site.me().setDomain("vod.cnzol.com");
 
     @Override
     public void process(Page page) {
-        List<String> links = page.getHtml().links().regex("https://my\\.oschina\\.net/flashsword/blog/\\d+").all();
+        List<String> links = page.getHtml().xpath("//[@id='list']/dl/dt/a[@class='img']/@href").regex("http://vod\\.cnzol\\.com/html/dongzuo/\\d+/\\d+\\.html").all();
         page.addTargetRequests(links);
-        page.putField("title", page.getHtml().xpath("//div[@class='blog-content']/div[@class='blog-heading']/div[@class='title']/text()").toString());
+        page.putField("title", page.getHtml().xpath("//[@id='show']/h3/text()").toString());
         if (page.getResultItems().get("title") == null) {
             //skip this page
             page.setSkip(true);
@@ -35,9 +31,9 @@ public class OschinaBlogPageProcessor implements PageProcessor {
     }
 
     public static void main(String[] args) {
-    	Spider spider = Spider.create(new OschinaBlogPageProcessor());
+    	Spider spider = Spider.create(new VodPageProcessor());
     	//ResultItems resultItems = spider.<ResultItems>get("https://my.oschina.net/flashsword/blog");
         //System.out.println(resultItems);
-    	spider.addUrl("https://my.oschina.net/flashsword/blog").run();
+    	spider.addUrl("http://vod.cnzol.com/html/dongzuo/").run();
     }
 }
